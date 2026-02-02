ba6626415bea7051e9c600fa46a90fc0e0c4fd50
Shreshth Mishra <shreshth.mishra@frauscher.com>
2026-02-02 17:19:15 +0530

    VTF-43: initialize Spring Boot project structure

---
+++ src/main/java/com/frauscher/ConfigurationValidationService/ConfigurationValidationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.frauscher.ConfigurationValidationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationValidationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationValidationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frauscher/ConfigurationValidationService/ConfigurationValidationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frauscher.ConfigurationValidationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationValidationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
